7da406cb65f1218d4692fe0f5412853643e1feae
osppro <39586345+osppro@users.noreply.github.com>
2023-03-15 16:47:11 +0300

    code

---
+++ search.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Live Search using AJAX</title>
+   <!-- Including jQuery is required. -->
+   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+   <!-- Including our scripting file. -->
+   <script type="text/javascript" src="script.js"></script>
+   <!-- Including CSS file. -->
+   <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<!-- Search box. -->
+   <input type="text" id="search" placeholder="Search" />
+   <br>
+   <b>Ex: </b><i>David, Ricky, Ronaldo, Messi, Watson, Robot</i>
+   <br />
+   <!-- Suggestions will be displayed in below div. -->
+   <div id="display"></div>
+</body>
+</html>
