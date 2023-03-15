501b3680236ff09d734c768038ee83fedb12c6a3
osppro <39586345+osppro@users.noreply.github.com>
2023-03-15 16:50:30 +0300

    Update db.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+//Database connection.
+$con = MySQLi_connect(
+   "localhost", //Server host name.
+   "root", //Database username.
+   "", //Database password.
+   "test" //Database name or anything you would like to call it.
+);
+
+//Check connection
+if (MySQLi_connect_errno()) {
+   echo "Failed to connect to MySQL: " . MySQLi_connect_error();
+}
+
+?>
